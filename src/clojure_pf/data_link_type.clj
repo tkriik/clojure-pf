370a7e76c6fe0873a8f86c92ab8683183922b48a
Tanel Kriik <tanel.kriik@outlook.com>
2016-10-12 21:29:39 +0300

    Add data link type utility

---
+++ src/clojure_pf/data_link_type.clj
@@ -0,0 +1,17 @@
+(ns clojure-pf.data-link-type
+  "Supported data link type level codes."
+  (:require [clojure.set :refer [map-invert]]))
+
+(def ^:private ^:const dlt-map
+  {:null      0     ; no link-layer encapsulation
+   :en10mb    1     ; ethernet (10Mb)
+   :ieee80211 105}) ; IEEE 802.11 wireless
+
+(def ^:private ^:const code-map
+  (map-invert dlt-map))
+
+(defn to-code [dlt]
+  (get dlt-map dlt))
+
+(defn from-code [code]
+  (get code-map code))
